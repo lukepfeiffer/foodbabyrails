9afe57ae40c5810b176b2411a1d3b4e934a69149
lukepfeiffer <lukerpfeiffer@gmail.com>
2018-10-13 17:31:02 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+addresses = ['686 Museum Rd', '1064 Center Dr', '444 Newell Dr', '1885 Stadium Rd', '330 Newell Dr']
+food = ['Pizza', 'Wings', 'Sandwiches', 'Burritos', 'Other']
+
+10.times do |n|
+  Event.create(
+    org_name: "Org#{n}",
+    building_name: "Building#{n}",
+    room_number: "33#{n}",
+    address_line_one: addresses.sample,
+    food_type: food.sample,
+    vegan: [true, false].sample,
+    vegetarian: [true, false].sample,
+    date: Date.today + [1, 2, 3].sample.days,
+    time: Time.now
+  )
+end
