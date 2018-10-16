a907a3b67df8749c0a25a88a84db0c0b27566338
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2018-10-16 12:46:03 -0400

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,27 +1,11 @@
 class Event < ActiveRecord::Base
 end
 
-
-#Hey man, good job getting this all done for us. I really appreciate it
-#beyond what words can describe. You're a real bro.
-#I'm really glad that this will all work out. How great is it
-#that we can just cheese this project?
-#Now I'm just writing pure trash for insertion numbers.
-#I might go and delete and reword Brandon's comments so that 
-#the deletion numbers are up there as well. Sweet.
-
-#Here's some more garbage for you. 
-#Hey hey hey. What's for dinner?
-#More garbage.
-
 #What's a banana's favorite dance move?
 #The split!
 
 #How did Ebenezer Scrooge win the football game?
 #The ghost of Christmas passed!
-
-#What's Heleln Keller's favorite color?
-#Velcro
 
 #Favorite movie?
 #Answer:
