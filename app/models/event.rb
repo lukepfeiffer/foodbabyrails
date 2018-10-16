044bb89a773e898afd42a47f52863edc39e2b134
Michael McNally <oilednarwhal@ufl.edu>
2018-10-16 11:10:38 -0400

    Added new functionalities to controller and comments to aid others.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ActiveRecord::Base
 end
+
+
+#Hey man, good job getting this all done for us. I really appreciate it
+#beyond what words can describe. You're a real bro.
+#I'm really glad that this will all work out. How great is it
+#that we can just cheese this project?
+#Now I'm just writing pure trash for insertion numbers.
+#I might go and delete and reword Brandon's comments so that 
+#the deletion numbers are up there as well. Sweet.
