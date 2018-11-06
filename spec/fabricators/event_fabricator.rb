f21461a54da0ef04cd555df6a66e9a9c6bde0d51
lukepfeiffer <lukerpfeiffer@gmail.com>
2018-11-06 17:26:57 -0500

    Setup test suite

---
+++ spec/fabricators/event_fabricator.rb
@@ -0,0 +1,14 @@
+Fabricator(:event) do
+  org_name "Org"
+  address_line_one "686 Museum Rd"
+  room_num "E2120"
+  building_name "Reitz Union"
+  food_type "Pizza"
+  vegan false
+  vegetarian false
+  date Date.today
+  time Time.now
+  lat 29.644920
+  long -82.357193
+  user_id 1
+end
