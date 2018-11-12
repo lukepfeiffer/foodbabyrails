b9bea3414dc19e43cda6c812401a106660a4ae77
brandon taylor <brandontaylor@ufl.edu>
2018-11-12 13:24:34 -0500

    added more useful information to some of the low level files

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,22 @@
+#This is another file that allows the
+#ruby backend to run as intended.
+
 require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+#Require the gems listed in Gemfile, including any gems
+#you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module FoodBaby
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    #Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    #Settings in config/environments/* take precedence over those specified here.
+    #Application configuration can go into files in config/initializers
+    #-- all .rb files in that directory are automatically loaded after loading
+    #the framework and any gems in your application.
   end
 end
