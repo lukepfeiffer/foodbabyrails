29736d2e3b36f881418775cb6f3f0ac86b2d142b
lukepfeiffer <lukerpfeiffer@gmail.com>
2018-11-21 16:25:24 -0500

    Create voting migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_035248) do
+ActiveRecord::Schema.define(version: 2018_11_21_212306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2018_10_21_035248) do
     t.string "password_digest", null: false
     t.string "password_reset_token", limit: 60, null: false
     t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
