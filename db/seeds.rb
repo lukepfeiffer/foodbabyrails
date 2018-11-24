50b7d36a536ec3410b9a02b07475e33a9f2da3b8
lukepfeiffer <lukerpfeiffer@gmail.com>
2018-11-24 08:12:03 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,24 +11,51 @@ User.delete_all
 puts "Deleting Events"
 Event.delete_all
 
-addresses = ['686 Museum Rd', '1064 Center Dr', '444 Newell Dr', '3150 Hull Rd', '330 Newell Dr']
-building = ["Reitz Union", "NEB", "CISE", 'SW REC', "TURL" ]
+addresses = [
+              'Reitz Union Hotel, Museum Road, Gainesville, FL, USA', 
+              'Library West, West University Avenue, Gainesville, FL, USA', 
+	      'UF MSL, Newell Drive, Gainesville, FL, USA', 
+	      'New Physics Building (NPB), Gale Lemerand Drive, Gainesville, FL, USA', 
+	      'Turlington Plaza, Newell Drive, Gainesville, FL, USA'
+	    ]
+lat = [
+	 "29.6462745",     #reitz
+	 "29.6514302",	   #Library West
+	 "29.6479842",     #Marston
+	 "29.6438149",     #NPB
+	 "29.6490825"      #Turlington
+      ]
+long = [
+	 "-82.34775339999999",   #reitz
+	 "-82.34290020000003",   #Library west
+	 "-82.34400219999998",   #Marston
+	 "-82.34969619999998",   #NPB
+	 "-82.34373529999999",   #Turlington
+       ]	
+buildings = [
+	      "Reitz Union",
+	      "Library West",
+	      "Marston",
+	      "NPB",
+	      "Turlington"
+            ]
+
 food = ['Pizza', 'Wings', 'Sandwiches', 'Burritos', 'Other']
 
 puts "Create Admin"
-User.create(username: "admin", password: "password", email: "admin@example.com")
-user = User.find_by(email: "admin@example.com")
+User.create(username: "admin", password: "password", email: "admin@ufl.edu", is_confirmed: true)
+user = User.find_by(email: "admin@ufl.edu")
 
 puts "Creating Events"
 5.times do |n|
-  address =  addresses[n] + ", Gainesville, FL US"
-  coor = Geocoder.search(address).first.coordinates
-  lat = coor[0]
-  long = coor[1]
+  address =  addresses[n]
+  latitude = lat[n]
+  longitude = long[n]
+  building = buildings[n]
 
   Event.create(
     org_name: "Org#{n}",
-    building_name: building[n],
+    building_name: building,
     room_number: "33#{n}",
     address_line_one: addresses[n],
     food_type: food[n],
@@ -36,8 +63,8 @@ puts "Creating Events"
     vegetarian: [true, false].sample,
     date: Date.today + [1, 2, 3, 4, 5][n].days,
     time: Time.now,
-    lat: lat,
-    long: long,
+    lat: latitude,
+    long: longitude,
     user_id: user.id
   )
 end
